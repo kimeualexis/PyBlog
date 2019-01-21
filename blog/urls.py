210f5745f612beb2263805f992142972501d8a60
kimeualexis <kimeualexis@gmail.com>
2019-01-21 11:26:06 +0300

    delete posta

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 	path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 	path('<int:pk>/comment/', CommentCreateView.as_view(), name='post-comment'),
 	path('<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-	path('<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+	#path('<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 	path('<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 	path('about/', views.about, name='blog-about'),
 ]
